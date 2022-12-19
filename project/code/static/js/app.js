b344d6ead09ef18a976d2f7841b213a10479ef14
bmilne64 <bronwynmilne64@gmail.com>
2022-12-19 18:31:20 -0500

    updates js file

--- project/code/static/js/app.js
+++ project/code/static/js/app.js
@@ -1,59 +1,69 @@
 
 
 d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(function(data){ 
-console.log(data);
+   
+    console.log(data);
 
     // Create an array of each id name
-    let ids = data.samples.map(row=>row.id)
-    
+    let ids = data.names;
+    var selecter = d3.select('#selDataset')
     // Append an option in the dropdown
     ids.forEach(function(id) {
-        d3.select('#selDataset')
-            .append('option')
-            .text(id)
+        selecter
+        .append('option')
+        .text(id)
+        .property('value', id)
         });
 
-    console.log(ids);
+    console.log(id);
 
-    graphs(ids);
-    demographics(ids);
+    var id = ids[0]
+    graphs(id);
+    demographics(id);
+
+});
 
     function graphs(id) {
+    
+    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+    
+    var samplesAll = data.samples;
+
+    var samplesFiltered = samplesAll.filter(var1=> var1.id == id);
+    console.log(samplesFiltered)
+    var samplesSelect = samplesFiltered[0];
     // Create arrays for sample_values, OTU ids, and OTU labels        
-    let sample_values = data.samples.map(row => row.sample_values);
-    let otu_ids = data.samples.map(row => row.otu_ids);
-    let otu_labels = data.samples.map(row => row.otu_labels);
+    var sample_values = samplesSelect.sample_values;
+    var otu_ids = samplesSelect.otu_ids;
+    var otu_labels = samplesSelect.otu_labels;
     console.log(sample_values);
 
     // sort values to get values highest to lowest
-    let values_sorted = sample_values.sort(function compareFunction(firstNum, secondNum) {return secondNum - firstNum; });
-    let values_10 = values_sorted.map(row => row.slice(0,10));
+    let values_10 = sample_values.slice(0,10).reverse();
     console.log(values_10);
 
     // sort ids to get values highest to lowest
-    let ids_sorted = otu_ids.sort(function compareFunction(firstNum, secondNum) {return secondNum - firstNum; });
-    let ids_10 = ids_sorted.map(row => row.slice(0,10));
+    let ids_10 = otu_ids.slice(0,10).reverse().map(ids_10 => `OTU ${ids_10}`);
     console.log(ids_10);
 
      // sort labels to get values highest to lowest
-     let labels_sorted = otu_labels.sort(function compareFunction(firstNum, secondNum) {return secondNum - firstNum; });
-     let labels_10 = labels_sorted.map(row => row.slice(0,10));
+     let labels_10 = otu_labels.slice(0,10).reverse();
      console.log(labels_10);
 
     let trace1 = {
     // .map(function(item) {return item.Name })
-        x: values_10[0],
-        y: ids_10[0].map(row => "OTU" + row),
-        text: labels_10[0],
+        x: values_10,
+        y: ids_10,
+        text: labels_10,
         type: "bar",
         orientation : 'h',
         width: .75,
-        transforms: [{
-            type: 'sort',
-            target: 'y',
-            order: 'descending',
-          }],
-        };
+        // transforms: [{
+        //     type: 'sort',
+        //     target: 'y',
+        //     order: 'descending',
+        //   }],
+        }
         
     
     let traceData = [trace1];
@@ -67,13 +77,13 @@ console.log(data);
 
     let trace2 = {
         // .map(function(item) {return item.Name })
-            x: otu_ids[0],
-            y: sample_values[0],
+            x: otu_ids,
+            y: sample_values,
             mode: 'markers',
-            text: otu_labels[0],
+            text: otu_labels,
             marker: {
-                size: sample_values[0],
-                color: otu_ids[0]
+                size: sample_values,
+                color: otu_ids
               }
             };
 
@@ -86,10 +96,12 @@ console.log(data);
     // Render the plot to the div tag with id "plot"
     Plotly.newPlot('bubble', traceData2, layout2);
 
-    }
+});}
 
 
     function demographics(id) {
+
+    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
 
     // Get the first ID to display on page
     let firstID = data.metadata[0]
@@ -103,51 +115,14 @@ console.log(data);
                 .append('h1')
                 // .merge(sampleMetadata)
                 .text(row => `${row.key} : ${row.value}`)
-                .style('font-size','12px');
-    };
-
-// On change to the DOM, call newData()
-// d3.selectAll("#selDataset").on("change", optionChanged());
+                .style('font-size','13px');
+    });}
 
 // Function called by DOM changes
 function optionChanged(newid) {
+
     graphs(newid);
     demographics(newid);
-
-};
-// function optionChanged() {
-//     let dropdownMenu = d3.select("#selDataset");
-//     // Assign the value of the dropdown menu option to a letiable
-//     let dataset = dropdownMenu.property("value");
-//     // Initialize an empty array for the country's data
-//     let data = [];
+    };
   
-//     if (dataset == 'australia') {
-//         data = australia;
-//     }
-//     else if (dataset == 'brazil') {
-//         data = brazil;
-//     }
-//     else if (dataset == 'uk') {
-//         data = uk;
-//     }
-//     else if (dataset == 'mexico') {
-//       data = mexico;
-//     }
-//     else if (dataset == 'singapore') {
-//         data = singapore;
-//     }
-//     else if (dataset == 'southAfrica') {
-//       data = southAfrica;
-//     }
-//   // Call function to update the chart
-//     updatePlotly(data);
-//   }
-  
-//   // Update the restyled plot's values
-//   function updatePlotly(newdata) {
-//     Plotly.restyle("pie", "values", [newdata]);
-//   }
-  
-//   init();
-});
+init();
